3f6a98333e54809669d73138e5f1baa0eb658a44
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2025-08-16 21:55:30 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectDB();
+
+// import express from "express";
+
+// const app = express()
+
+// // ;  add semicolor before iife just in case
+// (async()=>{
+
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on('error', (error)=>{
+//             console.error("Error: ", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error('Error', error)
+//         throw error
+//     }
+// })()
