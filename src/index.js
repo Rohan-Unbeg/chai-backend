0859acedfdf30bda15fa3b6e8e31678c19c4f2dd
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2025-08-23 00:45:37 +0530

    file upload using cloudinary, multer

--- src/index.js
+++ src/index.js
@@ -5,37 +5,15 @@ import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-
-
-
-
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running on port ${process.env.nPORT}` )
-    })
-})
-.catch((error)=>{
-    console.log("mongodb connection failed! ",error);
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongodb connection failed! ", error);
+  });
 
 // import express from "express";
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,45 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded successfully
+    console.log(
+      "file has been uploaded on cloudinary successfully",
+      response.url
+    );
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
+
+// temp code
+// cloudinary.v2.uploader
+//   .upload(
+//     "https://upload.wikimedia.org/wikipedia/commons/0/01/Charvet_shirt.jpg",
+//     {
+//       public_id: "wiki_shirt",
+//       quality_analysis: true,
+//       colors: true,
+//       categorization: "google_tagging",
+//       auto_tagging: 0.8,
+//     }
+//   )
+//   .then((result) => console.log(result));
